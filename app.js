37a1efcdb4420c856df2b4d87930485edfeff55d
yashj2022 <111694454+yashj2022@users.noreply.github.com>
2022-09-25 01:04:31 +0530

    finnal change

--- app.js
+++ app.js
@@ -1,62 +1,53 @@
-const billAmt = document.querySelector("#bill");
-const cashAmt = document.querySelector("#cash");
-const button = document.querySelector("#button");
-const errMsgDiv = document.querySelector("#messageDiv");
-const NoOfNotes = document.querySelectorAll(".NoOfNotes");
-const nextBtn = document.querySelector("#next-btn");
-const cashGivenDiv = document.querySelector("#cash-given");
-const table = document.querySelector("#table");
-const caption = document.querySelector("#caption");
-const notesArr = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-btn");
+const errorMessage = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+const changeTable = document.querySelector(".change-table");
 
-button.addEventListener("click", () => {
-    const billValue = parseInt(billAmt.value);
-    const cashValue = parseInt(cashAmt.value);
-    hideErrMsg();
-    table.style.display = "none";
-    if (billValue > 0) {
-        if (cashValue >= billValue) {
-            const returnAmt = cashValue - billValue;
-            console.log(returnAmt);
-            calculateAmt(returnAmt);
-            table.style.display = "block";
-            caption.innerHTML = `<h3>Change to Return : ₹${returnAmt}</h3>`;
-        } else {
-            errMsgHandler("You need to give more money to pay bill.");
-        }
+const availabeNotes = ["2000", "500", "100", "20", "10", "5", "1"];
+changeTable.style.visibility = "hidden";
+function validateBillAndCashAmount() {
+  changeTable.style.visibility = "visible";
+  var billAmountINNum = Number(billAmount.value);
+  var cashGivenINNum = Number(cashGiven.value);
+  // console.log(billAmountINNum > 0 && cashGivenINNum >= 0);
+  hideMessage();
+
+  if (billAmountINNum > 0 && cashGivenINNum >= 0) {
+    if (cashGivenINNum > billAmountINNum) {
+      const amountToBeReturned = cashGivenINNum - billAmountINNum;
+      calculateChange(amountToBeReturned);
+    } else if (cashGivenINNum === billAmountINNum) {
+      changeTable.style.visibility = "hidden";
+      errorMessage.style.display = "block";
+      errorMessage.innerText = "do not need to return any amount😀";
     } else {
-        errMsgHandler("Please enter valid bill amount!");
+      changeTable.style.visibility = "hidden";
+      showMessage("Do you Wanna wash plates 😄😆");
     }
-});
-nextBtn.addEventListener("click", () => {
-    hideErrMsg();
-    if (billAmt.value > 0) {
-        cashGivenDiv.style.display = "flex";
-        nextBtn.style.display = "none";
-    } else {
-        errMsgHandler("Please enter valid bill amount");
-    }
-});
-
-function hideErrMsg() {
-    errMsgDiv.style.display = "none";
+  } else {
+    changeTable.style.visibility = "hidden";
+    showMessage("Invalid  Amount");
+  }
 }
 
-function calculateAmt(returnAmt) {
-    for (let i = 0; i < notesArr.length; i++) {
-        const notes = Math.trunc(returnAmt / notesArr[i]);
-        returnAmt = returnAmt % notesArr[i];
-        NoOfNotes[i].innerText = notes;
-
-        if (notes > 0) {
-            NoOfNotes[i].style.backgroundColor = "#F0F8FF";
-            NoOfNotes[i].style.color = "black";
-        }
-    }
+function hideMessage() {
+  errorMessage.style.display = "none";
 }
 
-//error msg function
-function errMsgHandler(msg) {
-    errMsgDiv.style.display = "block";
-    errMsgDiv.innerText = msg;
+function calculateChange(amountToBeReturned) {
+  for (let i = 0; i < availabeNotes.length; i++) {
+    const numberOfNotes = Math.trunc(amountToBeReturned / availabeNotes[i]);
+
+    amountToBeReturned = amountToBeReturned % availabeNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+  }
 }
+
+function showMessage(message) {
+  errorMessage.style.display = "block";
+  errorMessage.innerText = message;
+}
+
+checkButton.addEventListener("click", validateBillAndCashAmount);
